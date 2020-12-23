adfec0174231a0285b9b8d82cf31d70176fceb3b
shangnich <76069218+shangnich@users.noreply.github.com>
2020-12-23 18:53:27 -0500

    Update pageHandler.php

--- pageHandler.php
+++ pageHandler.php
@@ -8,7 +8,7 @@ Programmer's Name: Shannon Reynolds
 Date: 12/21/20
 Synopsis: This page is the HTML for the Registration for my Blog
   */
-$servername = "127.0.0.1:56376 via TCP/IP";//Says where the server is
+$servername = "localhostdb";//Says where the server is
 $username = "shangreyn";//says what the username for the server is
 $password = "trythis1today";//Says what the password for the server is
 $dbname = "activity1";//Gives the Database name
