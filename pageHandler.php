32cff9037b53b49805d6ccd286deb6f87206d2d5
shangnich <76069218+shangnich@users.noreply.github.com>
2020-12-23 18:52:20 -0500

    Update pageHandler.php

--- pageHandler.php
+++ pageHandler.php
@@ -7,10 +7,10 @@ Module Version: 1
 Programmer's Name: Shannon Reynolds
 Date: 12/21/20
 Synopsis: This page is the HTML for the Registration for my Blog
-  
-$servername = "localhost";//Says where the server is
-$username = "root";//says what the username for the server is
-$password = "root";//Says what the password for the server is
+  */
+$servername = "127.0.0.1:56376 via TCP/IP";//Says where the server is
+$username = "shangreyn";//says what the username for the server is
+$password = "trythis1today";//Says what the password for the server is
 $dbname = "activity1";//Gives the Database name
 
 // Create connection
@@ -19,36 +19,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}*/
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
-
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-        continue;
-    }
-    
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 
-$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
-
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-mysqli_close($link);
 // Data from form
 $First_Name=$_POST['FirstName'];
 $Last_Name=$_POST['LastName'];
